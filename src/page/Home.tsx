8063abf87b34d93518fefa0632c09c954e327517
Alexandra Egorova <egorova.aleksandra@gmail.com>
2024-01-05 18:08:19 +0200

    fixed some bugs

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import SideBar from "../components/SideBar";
 import Table from "../components/Table";
 
 const Home = () => {
-  const [entity, setEntity] = useState<number>(123740);
+  const entity = 123740;
 
   return (
     <div className='wrapper'>
